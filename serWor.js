9898607115fcfc975aabf13be2332e7d32a36e29
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2020-11-15 22:58:38 -0500

    Adding-Links - Progressive Web App Modify

--- serWor.js
+++ serWor.js
@@ -1,5 +1,5 @@
 self.importScripts('sw-toolbox.js')
-toolbox.precache(['https://ccps-english.netlify.app/images-icons/index.html', ''])
+toolbox.precache(['index.html', ''])
 toolbox.router.get('/*', toolbox.networkFirst, {
  networkTimeoutSeconds: 5
 })
